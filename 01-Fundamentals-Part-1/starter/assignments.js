e5e4388f9ed9674657da85b00db1010aeb091a49
Ramona Lazdina <ramona.lazdina@helmes.com>
2023-11-07 14:46:18 +0200

    added assignments.js file

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,6 @@
+let country = "Latvia";
+let continent = "Eurasia";
+let population = 2;
+console.log(country);
+console.log(continent);
+console.log(population);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,29 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is fun!');
+console.log(80 + 8 - 10 + 2);
 
-console.log(80 + 8 - 10);
+console.log("Jonas");
+//Value-> smallest unit of info
+//Store values in variables:
+let firstName = "Ram";
+console.log(firstName);
+console.log(firstName);
+
+/*
+Variables notes
+    naming conventions:
+        camelCase
+        can start _ and $
+        uppercase - reserved for constants (PI = 3.14....)
+        cant start with a number
+        cant contain &,
+        cant use reserved keywords (ex: new)
+        should be descriptive
+*/
+
+
+
+/*
+Data Types
+
+*/
+
