eb454aa6e4e66c55e93f57ce62ed576ad93722fa
Ramona Lazdina <ramona.lazdina@helmes.com>
2023-11-23 19:25:25 +0200

    until for-of loop

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,8 +1,277 @@
 'use strict';
+/*
+//ARRAY DESTRUCTURING
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+let [main, , secondary] = restaurant.categories; //JUST SKIP!
+console.log(main, secondary);
+
+//Switching variables
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+[main, secondary] = [secondary, main]; //Raassigning values using destructuring
+console.log(main, secondary);
+const [starter, mainCourse] = restaurant.order(2, 1);
+console.log(starter, ',', mainCourse);
+
+//Nested destructuring
+const nested = [2, 4, [5, 6]];
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+//Default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
+
+
+
+//DESTRUCTURING OBJECTS
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: 'Via del Sole, 21',
+  starterIndex: 1,
+});
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName, //changing name of key
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+//Default Values
+const { menu = [], starterMenu: starters = [] } = restaurant; //setting default value
+console.log(menu, starters);
+
+//Mutating variables while destructing objects
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj); // if start with curly braces - expected block
+console.log(a, b);
+
+//Nested objects
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+
+
+//SPREAD OPERATOR - works on all iterables (arrays, string, ) BUT NOT ON OBJECTS(it is not iterable)
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const newArr = [1, 2, ...arr]; //SPREAD operator writes each value separately
+console.log(newArr);
+console.log(...newArr);
+console.log(1, 2, 3, 4, 5);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+//Copy Array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+//Join 2 arrays
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+const str = 'Jonas';
+const letters = [...str, ' ', 'S.'];
+console.log(...str);
+
+//const ingredients = [
+//   prompt("Let's make pasta! Ingredient 1?"),
+//   prompt(' Ingredient 2?'),
+//   prompt('Ingredient 3a?'),
+// ];
+// restaurant.orderPasta(...ingredients);
+
+//Objects
+const newRestaurant = { foundingYear: 1998, ...restaurant, founder: 'Lui' };
+console.log(newRestaurant);
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurant.name);
+console.log(restaurantCopy.name);
+
+
+//REST PATERNS and PARAMETERS
+// pack into another array; always have to be last in destructuring
+//1)Destructuring rest pattern
+const arr = [1, 2, ...[3, 4]]; //SPREAD cus on the right side of =
+//REST, cus on left side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+//Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(sat, weekdays);
+
+//2)Functions and rest pattern = rest parameters
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+  return sum;
+};
+add(2, 3);
+add(2, 3, 4, 5);
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
+
+
+//SHORT CIRCUITING -> && and ||
+//Use any data type
+//Can return any data type
+//Short-circuiting evaluation - in || => if fist value is truthy => returns first value (doesnt even evluate the second)
+console.log('-----||------');
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null); //returns null
+console.log(undefined || 0 || null || '' || 'Hello' || 23 || null);
+//restaurant.numGuests = 0;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('-----&&------');
+console.log(0 && 'Jonas'); //short circuits when first is falsy
+console.log(7 && 'Jonas'); //when everything is truthy => returns last value. Only true when all operands are false
+console.log('Hello' && 23 && null && 'Ram');
+
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach');
+}
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach'); //if .orderPizza is falsy => it doesnt evn evaluate second statement
+
+
+//NULLISH COALESCING OPERATOR
+restaurant.numGuests = 0;
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+
+// LOGICAL ASSIGNMENT Operators - ||= ??= &&=
+const rest1 = {
+  name: 'Capri',
+  // numGuests: 20,
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+// nullish assignment operator (null or undefined)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+// AND assignment operator = assign a value to variable if current value is truthy 
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
+
+//Challenge
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const [players1, players2] = game.players;
+console.log(players1, players2);
+const [gk, ...fieldPlayers] = players1;
+console.log(fieldPlayers);
+console.log(gk);
+
+const allPlayers = [...players1, ...players2];
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+const printGoals = function (...playerNames) {
+  let sum = playerNames.length;
+  for (let i = 0; i < playerNames.length; i++) {
+    console.log(playerNames[i], ', total sum of all: ', sum);
+  }
+};
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals(...game.scored);
+team1 > team2 && console.log('More likely to win: ', game.team1);
+team2 > team1 && console.log('More likely to win: ', game.team2);
+
+*/
 
 // Data needed for first part of the section
 const restaurant = {
@@ -26,4 +295,48 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    //destructuring as an argument
+    console.log(
+      `Order recieved! ${this.starterMenu[starterIndex]}, ${this.mainMenu[mainIndex]}
+      will be delivered ${time} at ${address}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}!`
+    );
+  },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
+
+//FOR-OF LOOP
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+for (const item of menu) console.log(item);
+
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`); //[0,'Pizza']
+}
+console.log(menu.entries()); //Array Iterator {}
+console.log(...menu.entries()); // [0, 'Pizza'], [1, 'Pasta'] etc
+
+//ENHANCED OBJECT LITERALS
+
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
