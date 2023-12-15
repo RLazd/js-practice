ddbaec07a627c52d7da41a1ebe5caf77dd9cd1d9
Ramona Lazdina <ramona.lazdina@helmes.com>
2023-12-15 15:27:16 +0200

    DOM exercises finished

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -1,10 +1,12 @@
 'use strict';
 
+const header = document.querySelector('.header');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal'); //.querySelectorAll() -> nodeList
 const btnScrollTo = document.querySelector('.btn--scroll-to');
+const allSections = document.querySelectorAll('.section'); //NodeLists do not update themselves
 const section1 = document.querySelector('#section--1');
 const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
@@ -12,29 +14,86 @@ const tabsContent = document.querySelectorAll('.operations__content');
 const nav = document.querySelector('.nav');
 
 ///////////////////////////////////////
+///////////////////////////////////////
 //
-// Modal window
-const openModal = function (e) {
+//Menu fade animation
+//mouseenter does not bubble, but mouseover does bubble
+const handleHover = function (e) {
+  //console.log(this, e.currentTarget);
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+//Passing "argument" into handler (event handler can have only 1 argument - event)
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+///////////////////////////////////////
+//
+// Page navigation (navigation bar)
+//EVENT DELEGATION
+// 1. Add eventListener to common parent element
+// 2. Detrmine what element originated event
+document.querySelector('.nav__links').addEventListener('click', function (e) {
   e.preventDefault();
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
 
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
-
-btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
-
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
-
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
+  //Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({
+      behaviour: 'smooth',
+    });
   }
 });
+
+//Not effective way to navigate:
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({
+//       behaviour: 'smooth',
+//     });
+//   });
+// });
+
+///////////////////////////////////////
+//
+//Sticky Navigation : the scroll event
+/* Bad practice (bad performance)
+const initialCoords = section1.getBoundingClientRect();
+window.addEventListener('scroll', function () {
+  //as soon as 1-section is reached -> make nav sticky
+  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
+*/
+const navHeight = nav.getBoundingClientRect().height;
+//console.log(navHeight);
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  //console.log(entry);
+  nav.classList.add('sticky');
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null, //entire view
+  threshold: 0,
+  rootMargin: `-${navHeight}px`, //when distance between the  start of the section and the viewport is the same as the navigation
+});
+headerObserver.observe(header);
 
 ///////////////////////////////////////
 //
@@ -73,42 +132,59 @@ btnScrollTo.addEventListener('click', function (e) {
 
 ///////////////////////////////////////
 //
-// Page navigation
-//EVENT DELEGATION
-// 1. Add eventListener to common parent element
-// 2. Detrmine what element originated event
-document.querySelector('.nav__links').addEventListener('click', function (e) {
-  e.preventDefault();
+//Reveal elements on scroll
+//const allSections = document.querySelectorAll('.section');
 
-  //Matching strategy
-  if (e.target.classList.contains('nav__link')) {
-    const id = e.target.getAttribute('href');
-    document.querySelector(id).scrollIntoView({
-      behaviour: 'smooth',
-    });
-  }
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  //console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target); //stop observing
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null, //root:null===viewport
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  //section.classList.add('section--hidden');
 });
 
-//Not effective way to navigate:
-// document.querySelectorAll('.nav__link').forEach(function (el) {
-//   el.addEventListener('click', function (e) {
-//     e.preventDefault();
-//     const id = this.getAttribute('href');
-//     console.log(id);
-//     document.querySelector(id).scrollIntoView({
-//       behaviour: 'smooth',
-//     });
-//   });
-// });
+///////////////////////////////////////
+//
+// Modal window
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
 
 ///////////////////////////////////////
 //
 // Tabbed component
 //Not good practice: tabs.forEach(t => t.addEventListener('click', () => console.log('TAB')));
-
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab'); //closest to not click on span element
-  console.log(clicked);
+  ///console.log(clicked);
 
   //Guard clause
   if (!clicked) return;
@@ -128,38 +204,124 @@ tabsContainer.addEventListener('click', function (e) {
 
 ///////////////////////////////////////
 //
-//Menu fade animation
-//mouseenter does not bubble, but mouseover does bubble
-const handleHover = function (e) {
-  console.log(this, e.currentTarget);
-  if (e.target.classList.contains('nav__link')) {
-    const link = e.target;
-    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
-    const logo = link.closest('.nav').querySelector('img');
+//Lazy loading images (to improve performance)
+const imgTargets = document.querySelectorAll('img[data-src]'); //selecting all el where img class has data-src property
+const loadImage = function (entries, observer) {
+  const [entry] = entries;
 
-    siblings.forEach(el => {
-      if (el !== link) el.style.opacity = this;
-    });
-    logo.style.opacity = this;
-  }
+  if (!entry.isIntersecting) return;
+
+  //Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+  // Cant remove the class too quickly, so: use addEventListener
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
 };
 
-//Passing "argument" into handler (event handler can have only 1 argument - event)
-nav.addEventListener('mouseover', handleHover.bind(0.5));
-nav.addEventListener('mouseout', handleHover.bind(1));
+const imgObserver = new IntersectionObserver(loadImage, {
+  root: null,
+  threshold: 0,
+  rootMargin: '-200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
 
 ///////////////////////////////////////
 //
-//Sticky Navigation : the scroll event
-/* Bad practice (bad performance)
-const initialCoords = section1.getBoundingClientRect();
-window.addEventListener('scroll', function () {
-  //as soon as 1-section is reached -> make nav sticky
-  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
-  else nav.classList.remove('sticky');
-});
-*/
+//Slider
+const slider = function () {
+  const slides = document.querySelectorAll('.slide');
+  //const slider = document.querySelector('.slider');
+  const btnLeft = document.querySelector('.slider__btn--left');
+  const btnRight = document.querySelector('.slider__btn--right');
+  const dotContainer = document.querySelector('.dots');
+  let curSlide = 0;
+  const maxSlide = slides.length - 1;
 
+  //...For testing:
+  // slider.style.transform = 'scale(0.4) translateX(-1200px)';
+  // slider.style.overflow = 'visible';
+  //...
+
+  //Functions
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        'beforeend',
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll('.dots__dot')
+      .forEach(d => d.classList.remove('dots__dot--active'));
+
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`) //Selecting element based on a value in []
+      .classList.add('dots__dot--active');
+  };
+
+  const goToSlide = function (slide) {
+    slides.forEach((s, i) => {
+      s.style.transform = `translateX(${100 * (i - slide)}%)`;
+    });
+    //1st 0%; 2nd - 100%, 200%, 300%
+  };
+
+  //Next  and previous slide
+  const nextSlide = function () {
+    if (curSlide === maxSlide) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  //Init
+  const init = function () {
+    goToSlide(0);
+    createDots();
+    activateDot(0);
+  };
+  init();
+
+  //Event Handlers
+  btnRight.addEventListener('click', nextSlide);
+  btnLeft.addEventListener('click', prevSlide);
+
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'ArrowLeft') prevSlide();
+    e.key === 'ArrowRight' && nextSlide();
+  });
+
+  dotContainer.addEventListener('click', function (e) {
+    if (e.target.classList.contains('dots__dot')) {
+      const { slide } = e.target.dataset; //destructuring
+      goToSlide(slide);
+      activateDot(slide);
+    }
+  });
+};
+slider();
+
+///////////////////////////////////////
 ///////////////////////////////////////
 //!LECTURES
 
@@ -181,8 +343,7 @@ console.log(document.documentElement); //entire html
 console.log(document.head);
 console.log(document.body);
 
-const header = document.querySelector('.header');
-const allSections = document.querySelectorAll('.section'); //NodeLists do not update themselves
+//const allSections = document.querySelectorAll('.section'); //NodeLists do not update themselves
 console.log(allSections);
 
 document.getElementById('section--1');
@@ -228,7 +389,7 @@ console.log(getComputedStyle(message).height); // works
 message.style.height =
   Number.parseFloat(getComputedStyle(message).height, 10) + 40 + 'px';
 
-document.documentElement.style.setProperty('--color-primary', 'orangered');
+//document.documentElement.style.setProperty('--color-primary', 'orangered');
 
 //ATTRIBUTES
 const logo = document.querySelector('.nav__logo');
@@ -345,3 +506,55 @@ console.log(h1.parentElement.children);
 [...h1.parentElement.children].forEach(el => {
   //if (el !== h1) el.style.transform = 'scale(0.5)';
 });
+
+//
+// STICKY NAVIGATION
+//The INTERSECTION OBSERVER API
+//callback function gest called when observed (target) element is intersecting the root element at the defined treshold
+/*
+const obsCallback = function (entries, observer) {
+  entries.forEach(entry => {
+    console.log(entry);
+  });
+};
+const obsOptions = {
+  root: null,
+  threshold: [0, 0.2], //percentage that we want to have visible in the root
+  //0 -> event will triger always when target element moves completely  out of he view and also as soon as it enters the view
+  //0.2 ->
+  //1 -> callback called when target is visible 100%
+};
+const observer = new IntersectionObserver(obsCallback, obsOptions);
+observer.observe(section1);
+*/
+
+//
+//LIFE-CYCLE
+/* Events
+Can see in NETWORK tab
+1.DOM Content loaded - as soon as html has been completely parsed (but all scripts have to be executed before). No images, just HTML and JS
+2.LOAD event - when all images, external resources (css) has been loaded
+3. BEFORE UNLOAD - before closing/reloading a page
+ */
+document.addEventListener('DOMContentLoaded', function (e) {
+  //Execute code only after the dom is ready: (but js script tag is in the end of html, so we dont need to listen to DOMContentLoaded)
+  console.log('HTML parsed & DOM tree built!');
+});
+
+window.addEventListener('load', function (e) {
+  console.log('Page fully loaded ', e);
+});
+
+// window.addEventListener('beforeunload', function (e) {
+//   //not sure this is working (asks for reloading, but not when closing page)
+//   e.preventDefault(); //in chrome not necessary
+//   console.log(e);
+//   e.returnValue = '';
+// });
+
+//
+//LOADING JS IN TML
+//1.Regular - never include in the head; just include it in the end of the body
+//2.Async - In head; Html parsing paralel with js script fetching; DOMContentLoaded does not wait for an async script. Good for old browsers.
+//Script not guaranteed to execute in order. Use when order doesnt matter (analytics)
+//3.Defer - Overall best execution!!!! In head; still async, but html parsing is not interrupted and execution of js happens in the end. DOMContentLoaded fires after defer script is executed and IN order.
